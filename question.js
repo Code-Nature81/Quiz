54ef0ac0547107a17fbf04d611f1b2a53cba3e00
Code-Nature81 <81328065+Code-Nature81@users.noreply.github.com>
2021-04-16 00:39:20 +0200

    Add files via upload

---
+++ question.js
@@ -0,0 +1,75 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quelle est la date du premier mandat de Jean Jaurès comme député de Carmaux ?",
+    answer: "1893",
+    options: [
+      "1891",
+      "1893",
+      "1895",
+      "1901"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quel est le nom du cours d'eau qui traverse Carmaux ?",
+    answer: "Cérou",
+    options: [
+      "Céret",
+      "Céroc",
+      "Cérou",
+      "Céoc"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quel est le nom du premier maire ouvrier de Carmaux ?",
+    answer: "Calvignac",
+    options: [
+      "Camboulives",
+      "Calmettes", 
+      "Malroux", 
+      "Calvignac"
+  
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quel était le surnom des mineurs de fond ?",
+    answer: "Gueules noires",
+    options: [
+      "Gueules noires",
+      "Gueules rouges",
+      "Gueules jaunes ",
+      "Gueules blanches"
+      
+    ]
+  },
+    {
+    numb: 5,
+    question: "Quel est le nom donné à la Sainte patronne des mineurs ?",
+    answer: "Sainte Barbe",
+    options: [
+      "Sainte Marguerite",
+      "Sainte Radegonde", 
+      "Sainte Sophie ", 
+      "Sainte Barbe"
+      
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
